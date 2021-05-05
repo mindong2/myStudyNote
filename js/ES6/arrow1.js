08accb1066d3da699ebe35faa2c542debe7da215
mindong2 <qlsnaos12@naver.com>
2021-05-05 22:30:41 +0900

    arrow1

---
+++ js/ES6/arrow1.js
@@ -0,0 +1,40 @@
+// arrow는 일반 함수를 보기 쉽게 만들어준다.//
+const fun = (arg) => console.log(`hello! ${arg}!`);
+/* const fun = function(){
+    console.log(`hello!`);
+}*/
+
+fun(`dongmin`); 
+////
+const names = [`dongmin`,`nico`,`lynn`];
+
+const hearts = names.map(function(item){
+    return item + `❤`;
+});
+
+console.log(hearts);
+
+//map은 각각의 아이템마다 함수를 호출
+
+////
+
+const names = [`dongmin`,`nico`,`lynn`];
+
+function addHeart(item){
+    return item + `❤`;
+}
+
+const hearts = names.map(addHeart);
+
+console.log(hearts);
+
+////
+
+const names = [`dongmin`,`nico`,`lynn`];
+
+const hearts = names.map(item => item + `❤`);
+//한줄안에 다 나오게 하면 return된다 -> implicit return. 
+//그렇지 않으면 {}추가.
+console.log(hearts);
+
+//셋은 다 같은 출력값.
