93845d5fee7680d4fdf4524113b15578f1bd48ab
mindong2 <qlsnaos12@naver.com>
2021-06-24 03:06:26 +0900

    헤더수정, 푸터추가, 컨텐트 추가

--- js/slide/script.js
+++ js/slide/script.js
@@ -2,14 +2,20 @@ const imgBox = document.querySelector('.imgBox'),
       slides = document.querySelectorAll('.slider_img');
 let i = 0;
 
+
 const NextSlide = () => {
+    slides[i].classList.remove("first");
     slides[i].classList.remove("active");
     i = (i + 1) % slides.length;
     slides[i].classList.add("active");
 }
+setInterval(NextSlide,5000);
 
 const PrevSlide = () => {
+    slides[i].classList.remove("first");
     slides[i].classList.remove("active");
     i = (i - 1 + slides.length) % slides.length;
     slides[i].classList.add("active");
 }
+
+
